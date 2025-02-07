36be9ecb62e5c86fa4628d16eb2ca1138f79ff57
sujal10022005 <144321575+sujal10022005@users.noreply.github.com>
2025-02-07 15:06:53 +0530

    Stack completed

---
+++ stack2/PostfixtoInfix.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+string solve(string val1,string val2,int ch){
+    string s = "";
+    s+=val1;
+    s.push_back(ch);
+    s+=val2;
+    return s;
+}
+int main(){
+    string s = "79+4*8/3-";// postfix operation
+    //we need 1 stack -> values
+    stack<string>val;
+    for(int i=0;i<s.length();i++){
+        // check if s[i] is a digit (0-9)
+        if(s[i]>=48 && s[i]<=57){// digit
+            val.push(to_string(s[i]-48));
+        }
+        else{ // s[i] it is -> *,/,+,-
+            // kaam karo
+            string val2 = val.top();
+            val.pop();
+            string val1 = val.top();
+            val.pop();
+            string ans = solve(val1,val2,s[i]);
+            val.push(ans);
+        }
+    }
+    cout<<val.top();   
+}
---
+++ stack2/tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+s.push_back(ch);

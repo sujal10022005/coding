2b8621afede727483c9a88d8872ab7ca68e8314b
sujal10022005 <144321575+sujal10022005@users.noreply.github.com>
2025-02-19 16:05:18 +0530

    Remove At Even  Index

---
+++ quene1/removeatEvenIndices.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<queue>
+#include<stack>
+using namespace std;
+void display(queue<int>& q){
+    int n = q.size();
+    for(int i=1;i<=n;i++){
+        int x = q.front();
+        cout<<x<<" ";
+        q.pop();
+        q.push(x);
+    }
+    cout<<endl;
+}
+void removeateven(queue<int>& q){
+    int n = q.size();
+    for(int i=0;i<n;i++){
+        if(i%2==0) q.pop();
+        else {
+            int x = q.front();
+            q.pop();
+            q.push(x);
+        }
+    }
+}
+int main(){
+    queue<int> q;
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    q.push(40);
+    q.push(50);
+    display(q);
+    removeateven(q);
+    display(q);
+}

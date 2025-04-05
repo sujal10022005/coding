6c87f00d628ce0fb55cfbae4ff7eb83d352a4cb9
sujal10022005 <144321575+sujal10022005@users.noreply.github.com>
2025-04-05 19:49:05 +0530

    created a node and display function for binary tree

---
+++ BT1/treenode.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+class node{// this is tree node
+    public:
+    int val;
+    node* left;
+    node* right;
+    node(int val){
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+void display(node* root){
+    if(root==NULL) return;
+    cout<<root->val<<" ";  
+    display(root->left);
+    display(root->right);
+}
+int main(){
+    node* a = new node(1); // root
+    node* b = new node(2);
+    node* c = new node(3);
+    node* d = new node(4);
+    node* e = new node(5);
+    node* f = new node(6);
+    node* g = new node(7);
+
+    a->left = b;
+    a->right = c;
+    b->left = d;
+    b->right = e;
+    c->left = f;
+    c->right = g;
+    display(a);
+}

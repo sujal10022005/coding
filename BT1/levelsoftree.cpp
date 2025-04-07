2d8c0c4ae550fdab50e9674a150a1fd23735d79d
sujal10022005 <144321575+sujal10022005@users.noreply.github.com>
2025-04-07 11:35:17 +0530

    Created no. of levels in tree function

---
+++ BT1/levelsoftree.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<limits.h>
+using namespace std;
+class node{// this is tree node
+    public:
+    int val;
+    node* left;
+    node* right;
+    node(int val){
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+void display(node* root){
+    if(root==NULL) return;
+    cout<<root->val<<" ";  
+    display(root->left);
+    display(root->right);
+}
+int levels(node* root){
+    if(root == NULL) return 0;
+    return 1+max(levels(root->left),levels(root->right));
+}
+int main(){
+    node* a = new node(1); // root
+    node* b = new node(2);
+    node* c = new node(3);
+    node* d = new node(4);
+    node* e = new node(5);
+    node* f = new node(6);
+    node* g = new node(7);
+
+    a->left = b;
+    a->right = c;
+    b->left = d;
+    b->right = e;
+    c->left = f;
+    c->right = g;
+    display(a);
+    cout<<endl;
+    cout<<levels(a);
+}
